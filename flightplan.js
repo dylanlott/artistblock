6f2eb8991cb3898778546433ac9215a45e1ee340
Dylan  Lott <lott.dylan@gmail.com>
2017-08-01 23:44:50 -0600

    flightplan push

--- flightplan.js
+++ flightplan.js
@@ -33,6 +33,27 @@ plan.remote('deploy', function (remote) {
   })
 })
 
+plan.local('rebuild', function (local) {
+  local.exec('git add .')
+  local.exec('git commit -am "Flightplan push"')
+  local.log('Committed successfully.')
+  local.exec('git push origin master')
+  local.log('Pushed to GitHub.')
+})
+
+plan.remote('rebuild', function (remote) {
+  remote.hostname()
+  remote.with('cd ' + remote.runtime.webRoot, function () {
+    remote.sudo('git pull origin master')
+    remote.log('Removing node_modules')
+    remote.sudo('rm -rf node_modules')
+    remote.log('Installing package dependencies')
+    remote.sudo('npm install')
+    remote.sudo('npm run build')
+    remote.log('Rebuild successful')
+  })
+})
+
 plan.remote('check', function (remote) {
   remote.exec('pm2 list')
   remote.exec('pm2 show builtright')
